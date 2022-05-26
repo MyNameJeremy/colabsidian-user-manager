MyNameJeremy/colabsidian-user-manager

commit 844c8322fc50917e4009e53c415ee5c32a36fc16

index.mjs => UserManager.js

UserManager.js
@@ -1,5 +1,3 @@
-//import cfg from './config.json' assert { type: 'json' };
-
 /** @returns {fasle} always false */
 function ERR(...data) {
   console.error(data);
@@ -22,7 +20,7 @@ function ensure(val, err_msg, true_return = val, false_return = false) {
   return !!val ? true_return : ERR(err_msg) || false_return;
 }
 
-class user_manager {
+class UserManager {
   /** @type {Object.<string, User>} */
   users = {};
   /** @type {Object.<string, GeneralKey>}*/
@@ -139,4 +137,6 @@ class user_manager {
  * @property {string} hash cryptographic key of the general key
  * @property {number} perms permissions of the general key
  */
+
+module.export = { UserManager, Types: { User, GeneralKey } };
 

commit 4a4d7b899883f557aa9291b18649ea08f44998f3

fix: 'module.exports' has an 's' at the end :'( issue was introduced in commit 844c8322fc50917e4009e53c415ee5c32a36fc16

UserManager.js
@@ -181,5 +181,5 @@ class GeneralKey {
   }
 }
 
-module.export = { UserManager, Types: { User, GeneralKey } };
+module.exports = { UserManager, Types: { User, GeneralKey } };
 